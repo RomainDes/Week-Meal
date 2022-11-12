a49db7a8d95df0062ff13148ed67bb738b13afa6
Maxime COSIALLS <79311075+Maxjuco@users.noreply.github.com>
2022-11-12 16:29:15 -0500

    adding Activity + Layout
    - Authentification
    - Planning
    - Recettes
    - Accueil

---
+++ app/src/main/java/com/example/weekmeal/Recette.java
@@ -0,0 +1,15 @@
+
+package com.example.weekmeal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Recette extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recette);
+    }
+}
