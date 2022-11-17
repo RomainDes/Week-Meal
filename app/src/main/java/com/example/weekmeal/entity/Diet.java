9e3dd82a6cddc793c29ae98f73143cd9813d4513
Maxime COSIALLS <79311075+Maxjuco@users.noreply.github.com>
2022-11-17 15:21:24 -0500

    Adding Diet Entity Class #8

---
+++ app/src/main/java/com/example/weekmeal/entity/Diet.java
@@ -0,0 +1,19 @@
+package com.example.weekmeal.entity;
+
+public class Diet {
+    private Integer id;
+    private String title;
+
+    public Diet(Integer id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
