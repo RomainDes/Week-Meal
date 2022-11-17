442e9001218285391f24059d59d26daa474245de
Maxime COSIALLS <79311075+Maxjuco@users.noreply.github.com>
2022-11-17 15:22:56 -0500

    Update MainActivity.java
    adding Default diet

--- app/src/main/java/com/example/weekmeal/MainActivity.java
+++ app/src/main/java/com/example/weekmeal/MainActivity.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.weekmeal.connector.FirebaseConnector;
+import com.example.weekmeal.entity.Diet;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    private void AddingDefaultDiet(){
+                FirebaseConnector fc = FirebaseConnector.instance();
+                fc.addDiet(new Diet(0, "Vegetarian"), this);
+                fc.addDiet(new Diet(1, "Vegan"), this);
+                fc.addDiet(new Diet(2, "Gluten Free"), this);
     }
 }
