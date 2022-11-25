563fd4599a4d01e56ed67e8b9fba27efd7373d99
Romain DESMARECAUX <78937225+RomainDes@users.noreply.github.com>
2022-11-25 00:05:02 -0500

    Page de choix

--- app/src/main/java/com/example/weekmeal/MakeAChoice.java
+++ app/src/main/java/com/example/weekmeal/MakeAChoice.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.List;
 
@@ -15,6 +18,8 @@ public class MakeAChoice extends AppCompatActivity {
     private MyRecyclerViewAdapter adapter;
 
     private List<Image> mViewImage;*/
+    private Button buttonChoose1;
+    private Button buttonChoose2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +36,24 @@ public class MakeAChoice extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         adapter = new MyRecyclerViewAdapter(this, );
         recyclerView.setAdapter(adapter);*/
+        buttonChoose1 = (Button) findViewById(R.id.choose1);
+        buttonChoose1.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                Intent intent = new Intent(MakeAChoice.this, yourchoice.class);
+                startActivity(intent);
+
+            }
+        });
+
+        buttonChoose2 = (Button) findViewById(R.id.choose2);
+        buttonChoose2.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                Intent intent = new Intent(MakeAChoice.this, yourchoice.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
