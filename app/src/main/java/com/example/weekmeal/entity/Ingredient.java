abe89a9a9c6628f403fb26f19f321df6c3976f02
Maxime COSIALLS <79311075+Maxjuco@users.noreply.github.com>
2022-11-26 11:30:18 -0500

    Adding quantity to ingredient #8

--- app/src/main/java/com/example/weekmeal/entity/Ingredient.java
+++ app/src/main/java/com/example/weekmeal/entity/Ingredient.java
@@ -6,10 +6,12 @@ public class Ingredient {
 
     private Integer id;
     private String title;
+    private String quantity;
 
-    public Ingredient(Integer id, String title) {
+    public Ingredient(Integer id, String title, String quantity) {
         this.id = id;
         this.title = title;
+        this.quantity = quantity;
     }
 
     public Ingredient(){
@@ -23,6 +25,9 @@ public class Ingredient {
     public String getTitle() {
         return title;
     }
+    public String getQuantity() {
+        return quantity;
+    }
 
     //toString():
     public String toString(){
@@ -33,6 +38,6 @@ public class Ingredient {
 
     //Converter from LinkedTreeMap to Ingredient:
     public static Ingredient convertLTM(LinkedTreeMap ingredientLTM){
-        return new Ingredient(new Integer(((Double) ingredientLTM.get("id")).intValue()), (String) ingredientLTM.get("title"));
+        return new Ingredient(new Integer(((Double) ingredientLTM.get("id")).intValue()), (String) ingredientLTM.get("title"), (String) ingredientLTM.get("quantity"));
     }
 }
