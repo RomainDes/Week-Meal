f7c2605d3e636af2c12d2e0ad826e6715d0aceac
copiright <antoine.munsch@groupe-esigelec.org>
2022-11-26 23:43:25 -0500

    Bouton retour recuperation des donnees  envoyees dans make a choice +  correction d'une incohérence

--- app/src/main/java/com/example/weekmeal/MakeAChoice.java
+++ app/src/main/java/com/example/weekmeal/MakeAChoice.java
@@ -1,11 +1,8 @@
 package com.example.weekmeal;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,19 +11,20 @@ import android.widget.Button;
 import com.example.weekmeal.entity.Diet;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MakeAChoice extends AppCompatActivity {
 
     private Button buttonReturn;
     private Button buttonChoose;
 
-
-    /*private RecyclerView recyclerView;
-    private MyRecyclerViewAdapter adapter;
-    private List<Image> mViewImage;*/
     private Button buttonChoose1;
     private Button buttonChoose2;
+
+    private  ArrayList<Boolean> toggleList = new ArrayList<>();
+
+    /*private RecyclerView recyclerView;
+        private MyRecyclerViewAdapter adapter;
+        private List<Image> mViewImage;*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,29 +32,48 @@ public class MakeAChoice extends AppCompatActivity {
         setContentView(R.layout.activity_make_achoice);
 
         buttonReturn = (Button) findViewById(R.id.choiceReturn);
-        buttonChoose = (Button) findViewById(R.id.choiceChoose);
+        //buttonChoose = (Button) findViewById(R.id.choiceChoose);
 
         Bundle extras = getIntent().getExtras();
-
         ArrayList<Diet> dietsTrue = new ArrayList<>();
         dietsTrue = (ArrayList<Diet>) getIntent().getSerializableExtra("dietsTrue");
+        toggleList = (ArrayList<Boolean>) getIntent().getSerializableExtra("toggleList");
         int mealPerDay = Integer.parseInt(extras.getString("mealPerDay"));
         int numberPeople = Integer.parseInt(extras.getString ("numberPeople"));
-        for (int i = 0 ; i< dietsTrue.size();i++){
-            Log.i("Diet : ",dietsTrue.get(i).getId()+" || "+dietsTrue.get(i).getTitle() +"");
-        }
 
-        Log.i("taille :  ",dietsTrue.size()+ "");
-        Log.i("meal :  ",mealPerDay +  "");
-        Log.i("persone :  ",numberPeople+ "");
 
 
     buttonReturn.setOnClickListener(new View.OnClickListener() {
         public void onClick(View view) {
             Intent intentReturn = new Intent(MakeAChoice.this, MainActivity.class);
+            intentReturn.putExtra("toggleList",toggleList);
+            intentReturn.putExtra("mealPerDay",mealPerDay +"");
+            intentReturn.putExtra("numberPeople", numberPeople+"");
             startActivity(intentReturn);
         }
     });
+
+    // partie de Romain avec les boutons
+        buttonChoose1 = (Button) findViewById(R.id.choiceChoose);
+        buttonChoose2 = (Button) findViewById(R.id.choose2);
+
+        buttonChoose1.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                Intent intent = new Intent(MakeAChoice.this, yourchoice.class);
+                startActivity(intent);
+
+            }
+        });
+
+        buttonChoose2.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                Intent intent = new Intent(MakeAChoice.this, yourchoice.class);
+                startActivity(intent);
+
+            }
+        });
 
 
         /*LinearLayoutManager layoutManager
@@ -69,24 +86,5 @@ public class MakeAChoice extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         adapter = new MyRecyclerViewAdapter(this, );
         recyclerView.setAdapter(adapter);*/
-        buttonChoose1 = (Button) findViewById(R.id.choose1);
-        buttonChoose1.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View view) {
-                Intent intent = new Intent(MakeAChoice.this, yourchoice.class);
-                startActivity(intent);
-
-            }
-        });
-
-        buttonChoose2 = (Button) findViewById(R.id.choose2);
-        buttonChoose2.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View view) {
-                Intent intent = new Intent(MakeAChoice.this, yourchoice.class);
-                startActivity(intent);
-
-            }
-        });
     }
 }
