feaa37ffff63131bbbfaac09b8318d7fecb01abf
Romain DESMARECAUX <78937225+RomainDes@users.noreply.github.com>
2022-12-14 19:33:40 -0500

    frontend end

--- app/src/main/java/com/example/weekmeal/MakeAChoice.java
+++ app/src/main/java/com/example/weekmeal/MakeAChoice.java
@@ -9,6 +9,8 @@ import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
 
 import java.util.List;
 
@@ -22,6 +24,21 @@ public class MakeAChoice extends AppCompatActivity {
     private Button buttonChoose2;
     private Button buttonReturn1;
     private Button buttonReturn2;
+
+    private RadioButton choice1;
+    private RadioButton choice2;
+    private RadioButton choice3;
+
+    private LinearLayout makeachoice;
+
+    private LinearLayout choice1Planning;
+    private LinearLayout choice2Planning;
+    private LinearLayout choice3Planning;
+
+    private Button back1;
+    private Button back2;
+    private Button back3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,6 +92,71 @@ public class MakeAChoice extends AppCompatActivity {
                 Intent intent = new Intent(MakeAChoice.this, yourchoice.class);
                 startActivity(intent);
 
+            }
+        });
+
+        makeachoice = (LinearLayout) findViewById(R.id.makeachoice);
+
+        choice1Planning = (LinearLayout) findViewById(R.id.choice1Planning);
+        choice1 = (RadioButton) findViewById(R.id.choice1);
+        choice1.setOnClickListener(new DoubleClickListener() {
+
+            @Override
+ 				public void onDoubleClick() {
+                    makeachoice.setVisibility(View.GONE);
+                    choice1Planning.setVisibility(View.VISIBLE);
+ 				}
+        });
+
+        back1 = (Button) findViewById(R.id.back1);
+        back1.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                makeachoice.setVisibility(View.VISIBLE);
+                choice1Planning.setVisibility(View.GONE);
+
+            }
+        });
+
+        choice2Planning = (LinearLayout) findViewById(R.id.choice2Planning);
+        choice2 = (RadioButton) findViewById(R.id.choice2);
+        choice2.setOnClickListener(new DoubleClickListener() {
+
+            @Override
+            public void onDoubleClick() {
+                makeachoice.setVisibility(View.GONE);
+                choice2Planning.setVisibility(View.VISIBLE);
+            }
+        });
+
+        back2 = (Button) findViewById(R.id.back2);
+        back2.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                makeachoice.setVisibility(View.VISIBLE);
+                choice2Planning.setVisibility(View.GONE);
+
+            }
+        });
+
+        choice3Planning = (LinearLayout) findViewById(R.id.choice3Planning);
+        choice3 = (RadioButton) findViewById(R.id.choice3);
+        choice3.setOnClickListener(new DoubleClickListener() {
+
+            @Override
+            public void onDoubleClick() {
+                makeachoice.setVisibility(View.GONE);
+                choice3Planning.setVisibility(View.VISIBLE);
+            }
+        });
+
+        back3 = (Button) findViewById(R.id.back3);
+        back3.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+
+                makeachoice.setVisibility(View.VISIBLE);
+                choice3Planning.setVisibility(View.GONE);
             }
         });
 
--- app/src/main/java/com/example/weekmeal/yourchoice.java
+++ app/src/main/java/com/example/weekmeal/yourchoice.java
@@ -6,26 +6,44 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class yourchoice extends AppCompatActivity {
 
 
     private Button buttonShopping;
+    private Button buttonPlanning;
     private Button buttonReturn1;
     private Button buttonReturn2;
+
+    private LinearLayout planningLayout;
+    private LinearLayout shoppingLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_yourchoice);
 
+        planningLayout = (LinearLayout) findViewById(R.id.planningLayout);
+        shoppingLayout = (LinearLayout) findViewById(R.id.shoppingLayout);
+
+
+        buttonPlanning = (Button) findViewById(R.id.planning);
+        buttonPlanning.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                planningLayout.setVisibility(View.VISIBLE);
+                shoppingLayout.setVisibility(View.GONE);
+
+            }
+        });
 
         buttonShopping = (Button) findViewById(R.id.shopping);
         buttonShopping.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View view) {
-                Intent intent = new Intent(yourchoice.this, yourchoice2.class);
-                startActivity(intent);
+                planningLayout.setVisibility(View.GONE);
+                shoppingLayout.setVisibility(View.VISIBLE);
 
             }
         });
